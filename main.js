6e797354aedae17d30be68c71cacbbcdc299d666
Juan camilo <juancchaparro97@gmail.com>
2021-01-08 13:42:27 -0500

    componente-carousel funcional con jsvanilla

---
+++ main.js
@@ -0,0 +1,46 @@
+let btn_left = document.getElementById("btn-left");
+let btn_right = document.getElementById("btn-right")
+
+
+addEventsCarousel()
+
+function addEventsCarousel(){
+    let buttonsLeft = document.getElementsByName("btn-left")
+    let buttonsRigth = document.getElementsByName("btn-right")
+
+    for(let i=0; i<buttonsLeft.length;i++){
+        buttonsLeft[i].addEventListener("click",function(){moveScroll(event,"left")})
+    }
+
+    for(let i=0; i<buttonsRigth.length;i++){
+        buttonsRigth[i].addEventListener("click",function(){moveScroll(event,"right")})
+    }
+}
+
+
+
+function moveScroll(event,type){
+
+    let boton = event.path[0];
+
+    let siblignsButton = boton.parentNode.parentNode.childNodes
+
+
+    for(let index in siblignsButton){
+
+        let cadena = siblignsButton[index].className
+        
+        if(cadena !== undefined && cadena.includes("carousel")){
+
+            if(type=="right"){
+                siblignsButton[index].scrollLeft += 420;
+            }
+            else if(type=="left"){
+                siblignsButton[index].scrollLeft -= 420;
+            }            
+        }
+        
+    }
+
+}
+
